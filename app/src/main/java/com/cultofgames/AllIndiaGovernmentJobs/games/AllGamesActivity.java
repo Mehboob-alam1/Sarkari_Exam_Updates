31c59ef9a18930da78c7711820b8758d94a76c3b
Mehboob-alam1 <ma8122576@gmail.com>
2023-06-11 19:44:33 +0000

    done

--- app/src/main/java/com/cultofgames/AllIndiaGovernmentJobs/games/AllGamesActivity.java
+++ app/src/main/java/com/cultofgames/AllIndiaGovernmentJobs/games/AllGamesActivity.java
@@ -6,7 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
 
+import com.cultofgames.AllIndiaGovernmentJobs.R;
 import com.cultofgames.AllIndiaGovernmentJobs.databinding.ActivityAllGamesBinding;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,7 @@ public class AllGamesActivity extends AppCompatActivity {
     private ArrayList<GameItems> allGamesList;
     private ActivityAllGamesBinding binding;
     private GameAdapter adapter;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,15 @@ public class AllGamesActivity extends AppCompatActivity {
         getWindow().setFlags(1024, 1024);
         setContentView(binding.getRoot());
 
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adViewA);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         allGamesList = GamesList.allGames();
 binding.btnBackGames.setOnClickListener(view -> {
